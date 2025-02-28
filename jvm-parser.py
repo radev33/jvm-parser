1f8fa0df98a73ef70558db24f1c5920512a80f70
Radev <alex1332bg@gmail.com>
2025-02-28 18:19:30 +0200

    Add files via upload
    Defined getPidInfo function

--- jvm-parser.py
+++ jvm-parser.py
@@ -4,8 +4,25 @@ import time
 
 #functions
 def getPidInfo(pid: int):
-    exec = subprocess.check_output(f"jinfo -sysprops {pid}", shell=True).decode()
-    print(exec)
+    try:
+        exec1 = subprocess.check_output(f"jinfo -sysprops {pid} | grep -E 'com\\.netfolio\\.(appname|fullname)='", shell=True).decode()
+        print(f"Info fromfrom pid {pid} \n")
+        print("jinfo -sysprops")
+        print(exec1)
+    except:
+        print(f"Error getting jinfo -sysprops from pid {pid} \n")
+    try:
+        exec2 = subprocess.check_output(f"jinfo -flags {pid} | grep -o 'XX:MaxHeapSize=[0-9]\\+'", shell=True).decode()
+        print("jinfo -flags")
+        print(exec2)
+    except:
+        print(f"Error getting jinfo -flags from pid {pid}")
+    try:
+        cmd1 = f"jstat -gc {pid} | awk 'NR==1 {{for(i=1; i<=NF; i++) header[i]=$i}} NR==2 {{for(i=1; i<=NF; i++) if(header[i]==\"S0C\" || header[i]==\"S1C\" || header[i]==\"OC\" || header[i]==\"EC\") print header[i] \": \" $i}}'"
+        exec3 = subprocess.check_output(cmd1, shell=True).decode()
+        print(exec3)
+    except:
+        print(f"Error getting jstat -gc from pid {pid}")
 
 #main body
 print("Executing jps")
@@ -19,13 +36,12 @@ for line in command.split('\n'):
     line = line.strip()
     if line:
         pid, processName = line.split()
+        if processName == "Jps":
+            continue
         pid_dictionary[pid] = processName
-
 print(pid_dictionary)
 
 for pid in pid_dictionary.keys():
-    if pid == list(pid_dictionary.keys())[0] or pid == list(pid_dictionary.keys())[1]:
-        continue
     getPidInfo(pid)
 
     
